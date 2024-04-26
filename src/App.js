c69a242e4081d5cf6e74b322111bb00157fc658a
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2024-04-26 11:24:15 +0300

    installed and migrated to tailwindand made ui adjustments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
     <>
-      <div className='login-card'>
-        <p className='login-prompt'>Login to Melodify.</p>
-        <input placeholder='Username'></input>
-        <input placeholder='Password'></input>
-        <button>Sign In</button>
+      <div className='h-screen flex justify-center items-center'>
+        <RegistrationPage />
       </div>
     </>
   );
